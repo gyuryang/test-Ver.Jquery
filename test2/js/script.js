44861f7cc22c4d95cf235a6aceb2293b279ccf89
gyuryang <loo1130@naver.com>
2019-09-19 20:41:11 +0900

    day0919

--- test2/js/script.js
+++ test2/js/script.js
@@ -26,14 +26,13 @@
 
 		window.onmousemove = e =>{
 			if(mouseup) return;
-
 			x = e.clientX-startX-2;
 			y = e.clientY-startY-2;
 			// console.log(x+" , "+y)
 			if(thisbox != "")
 				only("."+thisbox).style = `position:absolute; left:${x}px; top:${y}px; z-index:1`;
 			if(!mouseup){
-				onemove();
+				onmove();
 			}
 		}
 	});
@@ -43,10 +42,10 @@
 		mouseup = true;
 		if(thisbox != ""){
 			let boxnum = Number(thisbox.split("x")[1]);
-			only("."+thisbox).style = `position:absolute; left:${thisX}px; top:${boxnum*50-50}px; transition:0.5s`;
+			only("."+thisbox).style = `left:592px; top:${boxnum*50-50}px; transition:0.2s`;
 		}
 		Run = true;
-		onemove();
+		onmove();
 	});
 	function check(){
 		let derect = new Array();
@@ -61,23 +60,21 @@
 		return derect;
 	}
 	// thisbox가 움직이는거
-	function onemove(){
+	function onmove(){
 		let arr = [];
 		arr = check();
+		console.log(arr);
 		let derect = arr[0];
-		// let secondDe = arr[1];
+		let secondDe = arr[1];
 		// 어디에도 속하지 않을  떄 다른 박스들 위치 유지
-		if(!derect){
-			let boxnum = Number(thisbox.split("x")[1]);
-			for(let i=boxnum+1; i<=all("#wrap div").length; i++){
-				only(".box"+i).style = `position:absolute; top:${i*50-50}px;`;
-			}
-		}
-		if((derect=="left"&&Run)||(derect=="right"&&Run)){
+		if((derect=="left"||derect=="right")&&secondDe=="bottom"){
+			secondDe = arr[0];
+			down(secondDe,true);
+		}else if((derect=="left"&&Run)||(derect=="right"&&Run)){
 			Run = mouseup;
 			pullUp();
 		}else if(derect=="bottom"){
-			down(!mouseup);
+			down(secondDe);
 		}/*else if(derect=="top"&&thisY-y > 25){
 			mouseup != true? up() : up(false);
 		}*/
@@ -85,43 +82,44 @@
 	// thisbox아닌 다른 박스들을 위로 땅기는거
 	function pullUp(){
 		let boxnum = Number(thisbox.split("x")[1]);
-
+		console.log("pullup");
 		minus = mouseup ? 100 : 50;
 		minus2 = !mouseup ? 100 : 50;
 
 		for(let i = boxnum+1; i<=all("#wrap div").length; i++){
-			only(".box"+i).style = `position:absolute; top:${i*50-minus}px;`;
+			only(".box"+i).style = `left:592px; top:${i*50-minus2}px; transition:0.2s`;
 		}
-		setTimeout(function(){
-			for(let i=boxnum+1; i<=all("#wrap div").length; i++){
-				only(".box"+i).style = `position:absolute; top:${i*50-minus2}px; transition:0.3s`;
-			}
-		},100);
 	}
 
-	function down(t = true){
+	function down(second,Exist = false){
 		let plusnum = 1;
 		if(((y-thisY)/25)>=3){
 			plusnum = ((y-thisY)/25)%2 == 0 ? ~~((y-thisY)/25/2) : ~~((y-thisY)/25/2)+1;
 		}
-		console.log(~~((y-thisY)/25)/2+" , plusnum = "+plusnum);
 		let boxnum = Number(thisbox.split("x")[1]);
 		let movenum = Number(selectbox.split("x")[1])+plusnum;
-		if(!t){
+		if(mouseup){
 			savenum = savenum > all("#wrap div").length ? all("#wrap div").length : savenum;
-			// only(".box"+boxnum).style = `position:absolute; top:${savenum*50-50}px;`;
 			return;
 		}
 		if((!Run&&savenum == movenum)||movenum>all("#wrap div").length)
 			return;
-		Run = mouseup;
-		savenum = movenum;
-		only(".box"+movenum).style = `position:absolute; top:${movenum*50-50}px;`;
-		setTimeout(function(){
-			only(".box"+movenum).style = `position:absolute; top:${movenum*50-100}px; transition:0.3s`;
-			only(".box"+movenum).className = "box"+(movenum-1);
+		if(Exist){
+			for(let i = boxnum+1; i<=all("#wrap div").length; i++){
+				only(".box"+i).style = `left:592px; top:${i*50-50}px; transition:0.2s`;
+			}
+			only(".box"+boxnum).style = `left:592px; top:${i*50-50}px; transition:0.2s`;
 			thisbox = "box"+movenum;
 			all(".box"+boxnum)[0].className = "box"+movenum;
-		},100)
+		}else if(second=="right"||second=="left"){
+			pullUp();
+		}
+		savenum = movenum;
+		only(".box"+movenum).style = `left:592px; top:${movenum*50-100}px; transition:0.2s`;
+		only(".box"+movenum).className = "box"+(movenum-1);
+		thisbox = "box"+movenum;
+		all(".box"+boxnum)[0].className = "box"+movenum;
+		Run = mouseup;
+
 	}
 })();
