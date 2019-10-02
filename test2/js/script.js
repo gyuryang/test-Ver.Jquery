b89914609a46718b4be2dad051c4a3622f42221f
gyuryang <loo1130@naver.com>
2019-10-02 17:07:16 +0900

    day1002

--- test2/js/script.js
+++ test2/js/script.js
@@ -11,29 +11,40 @@
 	arr,
 	list,
 	x,
-	y;
-	$(document).on("mousedown","#wrap div",function(e){
+	y,
+	m=0,
+	distance = [];
+
+	distance.push(0);
+	for(let i=0; i<=$("#wrap section").length; i++){
+		m = m == 0? 8 : m+$(".box"+i).height()+16;
+		distance.push(m);
+		$(".box"+(i+1)).css('top',m);
+	}
+	$(document).on("mousedown","#wrap section",function(e){
 		thisbox = $(this).attr('class');
 		selectbox = thisbox;
 		startnum = Number(thisbox.split("x")[1]);
 
 		list = [];
-		let divlist = Array.from(document.querySelectorAll("#wrap div"));
+		let divlist = Array.from(document.querySelectorAll("#wrap section"));
 		for(let i=0; i<divlist.length; i++){
 			list.push(divlist[i].className);
 		}
-		console.log(list);
+		// console.log(list);
 		let startX = e.offsetX;
 		let startY = e.offsetY;
+		console.log(startX + " , " + startY);
+		console.log(e.clientX + " , " + e.clientY);
 
 		thisX = e.clientX - startX - 2;
 		thisY = e.clientY -	startY - 2;
-		console.log(thisX);
+		// console.log(thisX);
 		mouseup = false;
 		document.querySelector("#wrap").style.position = "";
 		$(window).mousemove(function(e){
 			if(mouseup) return;
-
+			
 			x = e.clientX - startX - 2;
 			y = e.clientY - startY - 2;
 
@@ -41,16 +52,16 @@
 
 			if(!mouseup)
 				onmove();
-		})	
+		})
 
 		$(document).mouseup(function(e){
 			if(mouseup) return;
 
 			mouseup = true;
-
+			  
 			let boxnum = Number(thisbox.split("x")[1]);
 			pullUp("mouse",arr);
-			$("."+thisbox).css({left : thisX+"px" , "top" : boxnum*50-50+"px","transition" : "0.1s","z-index" : "0"}); // position을 계속 지정하면 left 지정 필요 X
+			$("."+thisbox).css({left : thisX+"px" , "top" : distance[boxnum]+"px","transition" : "0.1s","z-index" : "0"}); // position을 계속 지정하면 left 지정 필요 X
 			// document.querySelector("."+thisbox).style.left = "";
 			Run = true;
 			onmove();
@@ -58,14 +69,17 @@
 	})
 
 	function check(){ // 현재 위치가 어디 방향인지를 체크함
+		let boxnum = Number(thisbox.split("x")[1]);
+		let btmhei = ($(".box"+(boxnum+1)).height() + 8)/2;
+		let tophei = ($(".box"+(boxnum-1)).height() + 8)/2;
 		let direct = new Array();
 		if(thisX - x > 125)
 			direct.push("left");
 		if(thisX - x < -125)
 			direct.push("right");
-		if(thisY - y > 25)
+		if(thisY - y > tophei)
 			direct.push("top");
-		if(thisY - y < -25)
+		if(thisY - y < -btmhei)
 			direct.push("bottom");
 		return direct;
 	}
@@ -73,7 +87,7 @@
 	function onmove(){ // 방향을 확인하고 어떤 함수를 호출 할 지 정함
 		arr = [];
 		arr = check();
-
+		
 		direc = arr[0];
 		let secondDe = arr[1];
 
@@ -94,7 +108,7 @@
 			list.map((v,idx)=>{
 				$("#wrap div").eq(idx).attr("class",list[idx]);
 			})
-			$(".box"+startnum).animate({left : thisX+"px","top" : startnum*50-50+"px"},100);
+			$(".box"+startnum).animate({left : thisX+"px","top" : distance[startnum]+"px"},100);
 		}
 		let boxnum = Number(thisbox.split("x")[1]);
 
@@ -102,20 +116,33 @@
 			if(array.length != 0 && i == startnum)
 				continue;
 			if(check != "")
-				$(".box"+i).animate({"top" : i*50-50+"px"},100);
+				$(".box"+i).animate({"top" : distance[i]+"px"},100);
 			else 
-				$(".box"+i).css({"top" : i*50-100+"px", "transition" : "0.1s"});
+				$(".box"+i).css({"top" : distance[i]+"px", "transition" : "0.1s"});
 		}
 	}
 
 	function bottomMove(){
-		console.log(direc);
-		let plusnum = 1;
-		if(~~((y-thisY)/25)>=3){
-			plusnum = ~~((y-thisY)/25)%2 == 0 ? ~~((y-thisY)/25/2) : ~~((y-thisY)/25/2)+1;
+		let heiarr = [];
+		for(let i=1; i<=$("#wrap section").length; i++){
+			heiarr.push(($(".box"+i).height()+8));
 		}
-		console.log(plusnum);
+		let plusnum = 0;
 		let boxnum = Number(thisbox.split("x")[1]);
+		let moveleng = y-thisY;
+		let idx = boxnum;
+		while(moveleng>0){
+			moveleng -= (heiarr[idx]/2);
+			plusnum++;
+			if(moveleng<=0) return;
+			moveleng -= (heiarr[idx]/2);
+			plusnum++;
+			idx++;
+		}
+		if(plusnum>=3){
+			plusnum = plusnum%2 == 0 ? plusnum/2 : ~~(plusnum/2)+1;
+		}
+		// console.log(plusnum);
 		let movenum = boxnum+plusnum;
 
 		if(mouseup){
@@ -128,11 +155,11 @@
 		savenum = movenum;
 
 		for(let i=movenum+1; i<=$("#wrap div").length; i++){
-			$(".box"+i).css({"top" : i*50-50+"px","transition" : "0.1s"})
+			$(".box"+i).css({"top" : distance[i]+"px","transition" : "0.1s"})
 		}
 
 		if(plusnum == 1){
-			$(".box"+movenum).css({"top" : movenum*50-100+"px","transition" : "0.1s"})
+			$(".box"+movenum).css({"top" : distance[movenum]+"px","transition" : "0.1s"})
 			$(".box"+movenum).attr('class',"cbox");
 
 			thisbox = "box"+movenum;
@@ -143,7 +170,7 @@
 			$(".box"+boxnum).attr('class','cbox');
 
 			for(let i=movenum+1; i<=$("#wrap div").length; i++){
-				$(".box"+i).css({"top" : i*50-50+"px","transition" : "0.1s"});
+				$(".box"+i).css({"top" : distance[i]+"px","transition" : "0.1s"});
 			}
 			for(let i=boxnum; i<=movenum; i++)
 				$(".box"+i).attr('class','box'+(i-1));
@@ -159,13 +186,28 @@
 	}
 
 	function topMove(){
-		let plusnum = 1;
-		if(~~((thisY-y)/25)>=3){
-			plusnum = ~~((thisY-y)/25)%2 == 0 ? ~~((thisY-y)/25/2) : ~~((thisY-y)/25/2)+1;
+		let heiarr = [];
+		for(let i=1; i<$("#wrap section").length; i++){
+			heiarr.push(($(".box"+i).height()+8));
 		}
+		let plusnum = 0;
+		let moveleng = thisY-y;
 		let boxnum = Number(thisbox.split("x")[1]);
+		let idx = boxnum;
+		while(moveleng>0){
+			moveleng -= (heiarr[idx]/2);
+			plusnum++;
+			if(moveleng<=0) return;
+			moveleng -= (heiarr[idx]/2);
+			plusnum++;
+			idx--;
+		}
+		if(plusnum>=3){
+			plusnum = plusnum%2 == 0 ? plusnum/2 : ~~(plusnum/2)+1;
+		}
+		
 		let movenum = boxnum-plusnum;
-		console.log(plusnum);
+		// console.log(plusnum);
 		if(mouseup){
 			savenum = savenum < 1? 1 : savenum;
 			return;
@@ -176,11 +218,11 @@
 		savenum = movenum;
 		
 		for(let i=boxnum+1; i<=$("#wrap div").length; i++){
-			$(".box"+i).css({"top" : i*50-50+"px","transition" : "0.1s"})
+			$(".box"+i).css({"top" : distance[i]+"px","transition" : "0.1s"})
 		}
 
 		if(plusnum == 1){
-			$(".box"+movenum).css({"top" : movenum*50+"px","transition" : "0.1s"})
+			$(".box"+movenum).css({"top" : distance[movenum]+"px","transition" : "0.1s"})
 			$(".box"+movenum).attr('class',"cbox");
 
 			thisbox = "box"+movenum;
@@ -192,7 +234,7 @@
 
 			for(let i=movenum; i<=$("#wrap div").length; i++){
 				let minus = i>=boxnum? 50 : 0;
-				$(".box"+i).css({"top" : i*50-minus+"px","transition" : "0.1s"});
+				$(".box"+i).css({"top" : distance[i]+"px","transition" : "0.1s"});
 			}	
 			for(let i=boxnum; i>=movenum; i--)
 				$(".box"+i).attr('class','box'+(i+1));
@@ -200,10 +242,10 @@
 			thisbox = "box"+movenum;
 			$(".cbox").attr('class',"box"+movenum);
 		}
-
+    
 		boxnum = Number(thisbox.split("x")[1]);
 		thisY = boxnum*50-50;
-
+    
 		Run = mouseup;
 	}
 })();
